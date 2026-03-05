75f8194106698357ae74cb0754df6a34a0570f12
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2026-03-05 20:14:54 +0000

    adding various context providers to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { routeTree } from './routeTree.gen';
+import { AuthProvider } from './state/auth-context';
+import { DataProvider } from './state/cardsDataContext';
+import { SortFilterProvider } from './state/sortFilterContext';
+import './index.css';
 
 const router = createRouter({ routeTree })
 
@@ -10,6 +15,16 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <AuthProvider>
+    <DataProvider>
+      <SortFilterProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </SortFilterProvider>
+    </DataProvider>
+  </AuthProvider>
 )
