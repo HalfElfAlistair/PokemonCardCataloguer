23862fcf5a0a78c1fe683cac68088602a6a084cb
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2026-04-06 18:56:20 +0100

    adding functionality to make request to external api when searching for a card, when on search page

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,5 +1,10 @@
 import { SearchIcon } from "../assets/svg/SearchIcon";
 import { useSortFilterStates } from '../state/sortFilterContext';
+import { useAuth } from '../state/auth-context';
+import { useData } from '../state/cardsDataContext';
+import { searchbyName, searchbyID } from "../api/functions";
+import type { Cards } from "../types/dataTypes";
+import { useLocation } from '@tanstack/react-router';
 interface TextInputProps {
     formID: string;
     inputType: string;
@@ -13,8 +18,57 @@ interface TextInputProps {
 }
 
 export const TextInput = ({ formID, inputType, labelText, inputClass, placeholderText, inputValue, updateFunction, validFormat, formatMessage }: TextInputProps) => {
-    const { updateSearchText } = useSortFilterStates();
+
+    // gets auth user
+    const { idToken } = useAuth();
+    const { data, setCardSearchData, setCardSearchStatus } = useData();
+    const { cards } = data;
+    const { pathname } = useLocation();
+
+    const { searchFormat, updateSearchText } = useSortFilterStates();
     const searchButtonText = 'Press to search for entered card name or id';
+    const searchButtonFunction = async (searchString: string) => {
+        const lowerCaseSearchString: string = searchString.toLowerCase();
+        await updateSearchText(lowerCaseSearchString);
+        if (pathname === '/search') {
+            if (idToken) {
+                let searchResult;
+                let dataToReturn;
+                setCardSearchStatus('loading')
+                if (searchFormat === 'id') {
+                    searchResult = await searchbyID({ idToken, searchString });
+                    const cardData = searchResult['data'];
+                    const cardID = cardData['id'];
+                    dataToReturn = { [cardID]: cardData };
+                } else {
+                    searchResult = await searchbyName({ idToken, searchString });
+                    dataToReturn = { ...searchResult['data'] };
+                }
+                if (searchResult.status === 200) {
+                    setCardSearchData(dataToReturn)
+                    setCardSearchStatus('default')
+                } else {
+                    setCardSearchStatus('error')
+                }
+            } else {
+                if (searchFormat === 'id') {
+                    const card = cards[lowerCaseSearchString];
+                    setCardSearchData({ [card.id]: card })
+                } else {
+                    let cardsMatchedByName: Cards = {};
+                    for (const cardID in cards) {
+                        const card = cards[cardID];
+                        const { name } = card;
+                        if ((name.toLowerCase().search(lowerCaseSearchString) >= 0)) {
+                            cardsMatchedByName[cardID] = card;
+                        }
+                    }
+                    setCardSearchData(cardsMatchedByName);
+                }
+            }
+        }
+    }
+
     const returnTextInput = () => {
         return (
             <input
@@ -36,7 +90,7 @@ export const TextInput = ({ formID, inputType, labelText, inputClass, placeholde
                     {returnTextInput()}
                     <button
                         className='btnTransparent searchButton'
-                        onClick={() => updateSearchText(inputValue)}
+                        onClick={() => searchButtonFunction(inputValue)}
                         title={searchButtonText}
                         aria-label={searchButtonText}
                     >
